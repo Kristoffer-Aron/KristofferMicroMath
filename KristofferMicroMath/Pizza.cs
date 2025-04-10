f4037ba5066b87836a57994d726c4c95ee971781
Alexpoelse1 <alexpoelsemart@gmail.com>
2025-04-10 12:56:47 +0200

    public

--- KristofferMicroMath/Pizza.cs
+++ KristofferMicroMath/Pizza.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace KristofferMicroMath
 {
-    internal class Pizza
+    public class Pizza
     {
             public int PizzaNr { get; set; }
             public String Name { get; set; }
