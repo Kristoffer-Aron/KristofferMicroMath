105eb8fc606c18b888118cc5221a77bf0a4796f3
Sofie Kobbernagel Jensen <sofie.jensen.03@gmail.com>
2025-04-10 12:46:31 +0200

    detaljer på klasse

--- KristofferMicroMath/Customer.cs
+++ KristofferMicroMath/Customer.cs
@@ -8,6 +8,21 @@ namespace KristofferMicroMath
 {
     public class Customer
     {
+        private static int count = 0;
+       
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+        public string Mail { get; set; }
 
+        public Customer(string name, string address, string phone, string mail)
+        {
+            Id = count++;
+            Name = name;
+            Address = address;
+            Phone = phone;
+            Mail = mail;
+        }
     }
 }
