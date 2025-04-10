c88685c4a6aed6d5df59fab472763d07aae03626
Kristoffer Aron Hendriksson <krh004@edu.zealand.dk>
2025-04-10 10:53:13 +0200

    Documentation Local

--- KristofferMicroMath/KristofferMicroMath.cs
+++ KristofferMicroMath/KristofferMicroMath.cs
@@ -21,7 +21,7 @@ namespace KristofferMicroMath
 
         public int Add3Numbers(int t1, int t2, int t3)
         {
-            return t1 + t2 + t3; //Add 3 numbers
+            return t1 + t2 + t3; //Add 3 numbers Adding Extra Comment
         }
     }
 }
